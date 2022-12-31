a7c81b8aaf5463e7d228aa0981cd27ca2af1ca88
Janeth Pérez <janeth_2124@hotmail.com>
2022-12-31 00:20:29 -0600

    new style navbar

--- vite-project/src/components/NavBar.jsx
+++ vite-project/src/components/NavBar.jsx
@@ -7,28 +7,49 @@ export const NavBar = () => {
   return (
     <>
          <header>
-         <nav>
-         <div className='header-name'>
-        <p>Jorge Luis Perez Canales</p>
-          <span>Sr Back-end Developer</span>
+         <nav className='navbar' id='navbar'>
+          <div className='navbar-brand'>
+          <p className='title-ing'> Ing. Jorge Luis Pérez Canales</p>
           </div>
-        <section className="header-icon-container">
-            <div className="icons">
-            <Link to={'/AboutMe'}><a href="/"><img src={account}/></a> </Link> 
-            <Link to={'/Experience'}>   <a href="/"><img src={idea}/></a> </Link> 
-            <Link to={'/Skills'}> <a href="/"><img src={skill}/></a> </Link> 
-            <Link to={'/ContactMe'}>    <a href="/"><img src={plane}/></a> </Link> 
-            </div>
-            <div className="header-span-container">
-               <Link to={'/AboutMe'}> <span>About Me</span> </Link>
-               <Link to={'/Experience'}> <span>Experience</span> </Link>
-               <Link to={'/Skills'}> <span>skills</span> </Link>
-               <Link to={'/ContactMe'}><span>Contact Me</span></Link> 
-            </div>
-        </section>    
+         <ul className='navbar-nav'>
+         <li className='nav-item'>
+             <Link to={'/AboutMe'}>
+                <img src={account} className='icon'/>
+                <a className='name'>About Me</a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+             <Link to={'/Experience'}>
+                <img src={idea} className='icon'/>
+                <a className='name'>Experience</a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+             <Link to={'/Skills'}>
+                <img src={skill} className='icon'/>
+                <a className='name'>Skills</a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+             <Link to={'/ContactMe'}>
+                <img src={plane} className='icon'/>
+                <a className='name'>Contact Me</a>
+              </Link>
+            </li>
+         </ul>
         </nav>
     </header>
 
     </>
   )
 }
+
+{/*
+          <ul>
+          <div className='Derecha'>
+           
+           
+           
+            
+            </div>
+</ul>*/}
